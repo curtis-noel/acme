ce4e08151f32c79f535d382b985011fc04122b4a
curtisnoel <curtis.noel@gmail.com>
2017-05-21 07:30:50 -0400

    Generated with JHipster 4.3.0

---
+++ src/main/webapp/app/app.module.ts
@@ -0,0 +1,52 @@
+import './vendor.ts';
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { Ng2Webstorage } from 'ng2-webstorage';
+
+import { AcmeSharedModule, UserRouteAccessService } from './shared';
+import { AcmeHomeModule } from './home/home.module';
+import { AcmeAdminModule } from './admin/admin.module';
+import { AcmeAccountModule } from './account/account.module';
+import { AcmeEntityModule } from './entities/entity.module';
+
+import { customHttpProvider } from './blocks/interceptor/http.provider';
+import { PaginationConfig } from './blocks/config/uib-pagination.config';
+
+import {
+    JhiMainComponent,
+    LayoutRoutingModule,
+    NavbarComponent,
+    FooterComponent,
+    ProfileService,
+    PageRibbonComponent,
+    ErrorComponent
+} from './layouts';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        LayoutRoutingModule,
+        Ng2Webstorage.forRoot({ prefix: 'jhi', separator: '-'}),
+        AcmeSharedModule,
+        AcmeHomeModule,
+        AcmeAdminModule,
+        AcmeAccountModule,
+        AcmeEntityModule
+    ],
+    declarations: [
+        JhiMainComponent,
+        NavbarComponent,
+        ErrorComponent,
+        PageRibbonComponent,
+        FooterComponent
+    ],
+    providers: [
+        ProfileService,
+        customHttpProvider(),
+        PaginationConfig,
+        UserRouteAccessService
+    ],
+    bootstrap: [ JhiMainComponent ]
+})
+export class AcmeAppModule {}
